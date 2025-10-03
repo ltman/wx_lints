7a07bf6ca6d7db8c7a943a68ae5075687386bba4
mon <skmon43@yahoo.com>
2025-10-03 21:51:22 +0700

    Add: maybeWhen fixer

--- lib/rules/disallow_maybe_when.dart
+++ lib/rules/disallow_maybe_when.dart
@@ -1,7 +1,10 @@
 import 'package:analyzer/dart/ast/ast.dart';
 import 'package:analyzer/dart/element/element.dart';
+import 'package:analyzer/diagnostic/diagnostic.dart';
 import 'package:analyzer/error/error.dart' as error;
 import 'package:analyzer/error/listener.dart';
+import 'package:analyzer/source/source_range.dart';
+import 'package:analyzer_plugin/utilities/change_builder/change_builder_dart.dart';
 import 'package:custom_lint_builder/custom_lint_builder.dart';
 
 class DisallowMaybeWhen extends DartLintRule {
@@ -10,7 +13,7 @@ class DisallowMaybeWhen extends DartLintRule {
         code: const LintCode(
           name: 'disallow_maybe_when',
           problemMessage: 'Usage of the maybeWhen(...) method is not allowed.',
-          errorSeverity: error.DiagnosticSeverity.ERROR,
+          errorSeverity: error.DiagnosticSeverity.WARNING,
         ),
       );
 
@@ -38,5 +41,30 @@ class DisallowMaybeWhen extends DartLintRule {
   }
 
   @override
-  List<Fix> getFixes() => [];
+  List<Fix> getFixes() => [_MaybeWhenFix()];
+}
+
+class _MaybeWhenFix extends DartFix {
+  @override
+  void run(
+    CustomLintResolver resolver,
+    ChangeReporter reporter,
+    CustomLintContext context,
+    Diagnostic analysisError,
+    List<Diagnostic> others,
+  ) {
+    context.registry.addMethodInvocation((MethodInvocation node) {
+      if (!node.sourceRange.intersects(analysisError.sourceRange)) return;
+
+      final changeBuilder = reporter.createChangeBuilder(
+        message: 'Use when(...) instead',
+        priority: 100,
+      );
+
+      changeBuilder.addDartFileEdit((DartFileEditBuilder builder) {
+        final SourceRange sourceRange = node.methodName.sourceRange;
+        builder.addSimpleReplacement(sourceRange, 'when');
+      });
+    });
+  }
 }
