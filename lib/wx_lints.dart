e67ee9c89d940c02638d3915f7b14d8900b2245e
Siwakorn K. <skmon43@outlook.com>
2026-07-07 10:48:46 +0700

    Remove: disallow_image_in_wx_button_v2
    Superseded by disallow_direct_asset_image, which already forbids
    AssetGenImage.image(...) everywhere, including inside WxButtonV2.

--- lib/wx_lints.dart
+++ lib/wx_lints.dart
@@ -1,6 +1,5 @@
 import 'package:custom_lint_builder/custom_lint_builder.dart';
 import 'package:wx_lints/rules/disallow_direct_asset_image.dart';
-import 'package:wx_lints/rules/disallow_image_in_wx_button_v2.dart';
 import 'package:wx_lints/rules/disallow_maybe_when.dart';
 import 'package:wx_lints/rules/disallow_static_image_tint.dart';
 import 'package:wx_lints/rules/disallow_tintable_image_no_tint.dart';
@@ -13,7 +12,6 @@ class _WxLinter extends PluginBase {
   List<LintRule> getLintRules(CustomLintConfigs configs) => [
     const DisallowMaybeWhen(),
     const PreferLowercaseHexColor(),
-    const DisallowImageInWxButtonV2(),
     const DisallowStaticImageTint(),
     const DisallowTintableImageNoTint(),
     const DisallowDirectAssetImage(),
